3a5bb7cc07aab5050a42b6b71dc54e9fd36122cd
Nick Perry <perry184@purdue.edu>
2019-07-31 11:07:17 -0400

    First commit, showing most fuel efficient car

---
+++ FuelEfficiency/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace FuelEfficiency
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double City { get; set; }
+        public double Highway { get; set; }
+        public double Combined { get; set; }
+
+        public static Car ParseFromCsv(string line)
+        {
+            var columns = line.Split(',');
+            //Thread.Sleep(200);
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                City = double.Parse(columns[5]),
+                Highway = double.Parse(columns[6]),
+                Combined = double.Parse(columns[7])
+            };
+
+        }
+    }
+}
---
+++ FuelEfficiency/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelEfficiency
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = GetCars("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Name}: {car.Combined}");
+            }
+            Console.ReadLine();
+        }
+
+        private static List<Car> GetCars(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv)
+                .OrderByDescending(c => c.Combined)
+                .Take(10)
+                .ToList();
+        }
+
+    }
+}
